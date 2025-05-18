ae817c9ceecf1c69154e06c47bb2999ad3e27af3
batiamida <laxonbs@gmail.com>
2025-05-18 15:00:51 +0300

    quantization + db setup

---
+++ setup_db.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+conn = sqlite3.connect("faces.db")
+cur = conn.cursor()
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS user_info (
+        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL
+    )
+""")
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS user_playlists (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        playlist_id TEXT NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES user_info(user_id)
+    )
+""")
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS face_info (
+        faiss_id INTEGER PRIMARY KEY,
+        user_id INTEGER NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES user_info(user_id)
+    )
+""")
+
+conn.commit()
+conn.close()
